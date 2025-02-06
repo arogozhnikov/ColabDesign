0eba66cc2185c41214fea85a1d3703c66d278b56
Alex Rogozhnikov <iamfullofspam@gmail.com>
2025-02-06 10:01:02 -0800

    rm unnecessary deps

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(include=['colabdesign*']),
     install_requires=['py3Dmol','absl-py','biopython',
-                      'chex','dm-haiku','dm-tree',
-                      'immutabledict','jax','ml-collections',
+                      'dm-haiku','dm-tree',
+                      'jax','ml-collections',
                       'numpy','pandas','scipy','optax','joblib',
                       'matplotlib'],
     include_package_data=True
